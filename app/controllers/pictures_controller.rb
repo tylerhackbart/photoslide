0620661afe2ea892d6440d53eefcdc13957603b1
tylerhackbart <t.hackbart@gmail.com>
2015-11-25 20:10:14 -0500

    Making pictures show at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,6 @@
 class PicturesController < ApplicationController
 
 	def index 
+		@pictures = Picture.all
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+Picture.create!(
+  :title  => "The old church on the coast of White sea",
+  :artist => "Sergey Ershov",
+  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+)
+
+Picture.create!(
+  :title  => "Sea Power",
+  :artist => "Stephen Scullion",
+  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+)
+
+Picture.create!(
+  :title  => "Into the Poppies",
+  :artist => "John Wilhelm",
+  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+)
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
